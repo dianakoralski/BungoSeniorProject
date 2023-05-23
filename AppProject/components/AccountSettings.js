ce9df9a324e954f9d96f4e39ede7c679124769aa
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2023-05-22 19:43:57 -0700

    accountsettings
    almost done

--- AppProject/components/AccountSettings.js
+++ AppProject/components/AccountSettings.js
@@ -1,13 +1,124 @@
+// import { Link } from '@react-navigation/native';
+// import React from 'react';
+// import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+// import LinkButton from './LinkButton';
+
+// function AccountSettings(props) {
+//     const options = [
+//         { id: 1, label: 'Account', goTo: 'Account' },
+//         { id: 2, label: 'Settings', goTo: 'Settings' },
+//         { id: 3, label: 'Notifications', goTo: 'Notifications' },
+//         { id: 4, label: 'Storage and Data', goTo: 'StorageAndData' },
+//         { id: 5, label: 'Help', goTo: 'Help' },
+//         // Add more options as needed
+//     ];
+
+//     const renderOption = ({ item }) => (
+//         <Text style={styles.option}>{item.label}</Text>
+//     );
+
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.title}>
+//                 Account Settings
+//             </Text>
+//             {/* <Text style={styles.name}>
+//                 Oscar Liu
+//             </Text>
+//             <Text style={styles.descr}>
+//                 Realtor At San Luis Obispo Realty
+//             </Text> */}
+//             <View style={styles.profileContainer}>
+//                 <Image
+//                 source={require('../assets/avatar-1577909.png')} // Replace with the actual image source
+//                 style={styles.profileImage}
+//                 />
+//                 <Text style={styles.name}>Oscar Liu</Text>
+//                 <Text style={styles.descr}>
+//                     Realtor At San Luis Obispo Realty
+//                 </Text>
+//             </View>
+//             <FlatList
+//                 data={options}
+//                 renderItem={({ item }) => (
+//                     <LinkButton
+//                     text={item.label}
+//                     color="black"
+//                     goTo={item.goTo}
+//                     fontSize={16}
+//                     />
+//                 )}
+//                 keyExtractor={(item) => item.id.toString()}
+//                 contentContainerStyle={styles.optionsContainer}
+//                 ItemSeparatorComponent={() => <View style={styles.separator} />}
+//             />
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         color: 'black',
+//     },
+//     title: {
+//         textAlign: 'center',
+//         fontSize: 24,
+//         marginTop: '10%',
+//     },
+//     name: {
+//         textAlign: 'center',
+//         fontSize: 16,
+//         marginTop: '2%',
+//     },
+//     profileContainer: {
+//         flexDirection: 'row',
+//         alignItems: 'left',
+//         marginTop: '5%',
+//     },
+//       profileImage: {
+//         width: 50,
+//         height: 50,
+//         borderRadius: 25, // Make it a circular image by setting borderRadius to half of width and height
+//         marginRight: 10,
+//     },
+//     descr: {
+//         fontSize: 12,
+//         flexDirection: 'column',
+//     },
+//     optionsContainer: {
+//         alignItems: 'center',
+//         marginTop: '20%',
+//     },
+//     separator: {
+//         height: 10, // Adjust the desired padding height between LinkButtons
+//     },
+// });
+
+// export default AccountSettings;
+
+import { Link } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import LinkButton from './LinkButton';
 
 function AccountSettings(props) {
+    // const options = [
+    //     { id: 1, label: 'Account', goTo: 'Account' },
+    //     { id: 2, label: 'Settings', goTo: 'Settings' },
+    //     { id: 3, label: 'Notifications', goTo: 'Notifications' },
+    //     { id: 4, label: 'Storage and Data', goTo: 'StorageAndData' },
+    //     { id: 5, label: 'Help', goTo: 'Help' },
+    //     // Add more options as needed
+    // ];
     const options = [
-        { id: 1, label: 'Account' },
-        { id: 2, label: 'Settings' },
-        { id: 3, label: 'Notifications' },
-        { id: 4, label: 'Storage and Data' },
-        { id: 5, label: 'Help' },
+        { id: 1, label: 'Account', goTo: 'Login' },
+        { id: 2, label: 'Settings', goTo: 'Login' },
+        { id: 3, label: 'Notifications', goTo: 'Login' },
+        { id: 4, label: 'Storage and Data', goTo: 'Login' },
+        { id: 5, label: 'Help', goTo: 'Login' },
         // Add more options as needed
     ];
 
@@ -20,17 +131,37 @@ function AccountSettings(props) {
             <Text style={styles.title}>
                 Account Settings
             </Text>
-            <Text style={styles.name}>
+            {/* <Text style={styles.name}>
                 Oscar Liu
             </Text>
             <Text style={styles.descr}>
                 Realtor At San Luis Obispo Realty
-            </Text>
+            </Text> */}
+            <View style={styles.profileContainer}>
+                <Image
+                    source={require('../assets/avatar-1577909.png')} // Replace with the actual image source
+                    style={styles.profileImage}
+                />
+                <View style={styles.textContainer}>
+                    <Text style={styles.name}>Oscar Liu</Text>
+                    <Text style={styles.descr}>
+                        Realtor At San Luis Obispo Realty
+                    </Text>
+                </View>
+            </View>
             <FlatList
                 data={options}
-                renderItem={renderOption}
-                keyExtractor={item => item.id.toString()}
+                renderItem={({ item }) => (
+                    <LinkButton
+                        text={item.label}
+                        color="black"
+                        goTo={item.goTo}
+                        fontSize={16}
+                    />
+                )}
+                keyExtractor={(item) => item.id.toString()}
                 contentContainerStyle={styles.optionsContainer}
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
             />
         </View>
     );
@@ -49,22 +180,33 @@ const styles = StyleSheet.create({
         marginTop: '10%',
     },
     name: {
-        textAlign: 'center',
+        textAlign: 'left',
         fontSize: 16,
         marginTop: '2%',
     },
+    profileContainer: {
+        flexDirection: 'row',
+        alignItems: 'flex-start', // Align the content to the top left
+        marginTop: '5%',
+    },
+    profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25, // Make it a circular image by setting borderRadius to half of width and height
+        marginRight: 10,
+    },
+    textContainer: {
+        flexDirection: 'column',
+    },
     descr: {
         fontSize: 12,
-        marginTop: '2%'
     },
     optionsContainer: {
         alignItems: 'center',
         marginTop: '20%',
     },
-    option: {
-        textAlign: 'center',
-        fontSize: 16,
-        marginBottom: 10,
+    separator: {
+        height: 10, // Adjust the desired padding height between LinkButtons
     },
 });
 
