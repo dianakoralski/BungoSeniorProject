1c138c9ddd8a1d1fde3833415c023f4f25f82e51
dianakoralski <dianakoralski@hotmail.com>
2023-05-22 19:41:18 -0700

    scrolling without taskbar for Home page

--- AppProject/components/Home.js
+++ AppProject/components/Home.js
@@ -1,104 +1,100 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import {Text, View, Button, FlatList, StyleSheet, Image} from 'react-native';
-import {Card, FAB} from 'react-native-paper';
+import { Text, View, Button, FlatList, StyleSheet, Image } from 'react-native';
+import { Card, FAB } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Taskbar from './Taskbar';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function Home(props) {
+  const navigation = useNavigation();
+  const data = [
+    { id: 1, title: 'First Title', body: 'First Body' },
+    { id: 2, title: 'Second Title', body: 'Second Body' },
+    { id: 3, title: 'Third Title', body: 'Third Body' }
+  ];
 
-   // const [data1, setData]= useState([])
-/*
-    useEffect(() => {
-        fetch('http://192.168.1.118:5000/users',
-        {method: 'GET'})
-        //.then(resp => resp.json)
-        .then(article => {setData(article)})
-    }, [])
-*/
-
-    const navigation = useNavigation();
-    const data = [
-        {id: 1, title: 'First Title', body: 'First Body'},
-        {id: 2, title: 'Second Title', body: 'Second Body'},
-        {id: 3, title: 'Third Title', body: 'Third Body'}
-    ]
-
-    const renderData = (item) =>{
-        return(
-          
-            <Card style = {styles.cardStyle}>
-                <Text style = {{fontSize: 20}}>{item.title}</Text>
-                <Text>{item.body}</Text>
-            </Card>
-          
-        )
-    }
+  const renderData = (item) => {
+    return (
+      <Card style={styles.cardStyle}>
+        <Text style={{ fontSize: 20 }}>{item.title}</Text>
+        <Text>{item.body}</Text>
+      </Card>
+    );
+  };
 
   return (
-    <View style ={{flex:1}}>
+    <View style={{ flex: 1 }}>
       <View style={styles.textBox}>
         <Image source={require('./bungotext.png')} style={styles.logo} />
-    </View>
+      </View>
 
-    <Taskbar></Taskbar>
-      <View>
-        <Text>Welcome back {props.route.params.email} !</Text> 
-        {/*change to name, not email */}
+      <View style={{ flex: 1 }}>
+        <Text style = {{fontSize: 30}}>Welcome back {props.route.params.email}!</Text>
+        {/* change to name, not email */}
 
         <FlatList
-          data ={data}
-          renderItem = {({item}) => {
-              return renderData(item)
+          data={data}
+          renderItem={({ item }) => {
+            return renderData(item);
           }}
-          keyExtractor = {item => `${item.id}`}
+          keyExtractor={(item) => `${item.id}`}
           numColumns={2}
         />
       </View>
 
-      <FAB
-        style = {styles.fab}
-        small = {false}
-        icon = "plus"
-        theme={{colors:{accent:"green"}}}
-        // onPress ={() => console.log("Pressed")}
-        onPress = {() => navigation.navigate('AccountSettings')}
-      />
-    </View>
-  )
+      <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+        <Taskbar />
+      </View>
 
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'flex-end',
+          position: 'absolute',
+          top: '7%',
+          right: '4%'
+        }}
+      >
+        <TouchableOpacity onPress={() => navigation.navigate('AccountSettings')}>
+          <Icon name="gear" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    cardStyle: {
-      marginTop: 10,
-      marginLeft: 10,
-      paddingTop: 10,
-      width: '45%',
-      height: '90%',
-      flexDirection: 'row'
-    },
-    fab:{
-        position: 'absolute',
-        margin: 16,
-        right: 0,
-        bottom: 0
+  cardStyle: {
+    marginTop: 10,
+    marginLeft: 10,
+    paddingTop: 10,
+    width: '45%',
+    height: '90%',
+    flexDirection: 'row'
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 70
+  },
+  textBox: {
+    marginTop: 20,
+    width: '100%',
+    height: '10%',
+    backgroundColor: '#191970',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logo: {
+    height: 75,
+    width: 220,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
-    },
-    textBox: {
-      marginTop: 20,
-      width: '100%',
-      height: '10%',
-      backgroundColor: '#191970',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logo: {
-      height: 75,
-      width: 220,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
-  });
-
-export default Home
+export default Home;
--- AppProject/components/Taskbar.js
+++ AppProject/components/Taskbar.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, Modal, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 const Taskbar = ({ activeItem, onItemClick }) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
+  const handleDropdownPress = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
+  const handleMenuItemPress = (item) => {
+    // Handle the press of a dropdown menu item here
+    console.log('Dropdown menu item pressed:', item);
+    // You can close the dropdown modal here or perform any other action
+    setIsDropdownVisible(false);
+  };
+
+  const dropdownData = [
+    { id: '1', title: 'Option 1' },
+    { id: '2', title: 'Option 2' },
+    { id: '3', title: 'Option 3' },
+  ];
 
   return (
     <View style={styles.container}>
@@ -16,7 +32,7 @@ const Taskbar = ({ activeItem, onItemClick }) => {
         ]}
         onPress={() => navigation.navigate('Login')}
       >
-        <Text style={styles.itemText}>Home</Text>
+        <Text style={styles.itemText}>Manage Listings</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
@@ -29,7 +45,31 @@ const Taskbar = ({ activeItem, onItemClick }) => {
         <Text style={styles.itemText}>About</Text>
       </TouchableOpacity>
 
-      {/* Add more taskbar items as needed */}
+      <TouchableOpacity style={styles.dropdownButton} onPress={handleDropdownPress}>
+        <Text style={styles.dropdownButtonText}>Menu</Text>
+      </TouchableOpacity>
+
+      <Modal visible={isDropdownVisible} animationType="fade" transparent>
+        <TouchableOpacity
+          style={styles.dropdownBackground}
+          onPress={handleDropdownPress}
+          activeOpacity={1}
+        >
+          <FlatList
+            style = {{marginTop: 100}}
+            data={dropdownData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.dropdownItem}
+                onPress={() => handleMenuItemPress(item)}
+              >
+                <Text style={styles.dropdownItemText}>{item.title}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </TouchableOpacity>
+      </Modal>
     </View>
   );
 };
@@ -38,7 +78,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     backgroundColor: '#BEBEBE',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     alignItems: 'center',
     height: 50,
   },
@@ -56,6 +96,31 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#333',
   },
+  dropdownButton: {
+    paddingHorizontal: 10,
+  },
+  dropdownButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  dropdownBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  dropdownItem: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#FFFFFF',
+    marginRight: 10 //change
+  },
+  dropdownItemText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
 });
 
-export default Taskbar;
+export default Taskbar
