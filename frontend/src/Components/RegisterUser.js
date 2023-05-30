9c18795824b2d81d0085268b55acae41574d2982
dianakoralski <dianakoralski@hotmail.com>
2023-05-29 18:17:58 -0700

    Cleaned up some files

--- frontend/src/Components/RegisterUser.js
+++
@@ -1,9 +0,0 @@
-import "./Signup.css";
-import axios from "axios";
-
-async function RegisterUser(newUser) {
-  await axios.post("http://localhost:5000/users", newUser);
-  window.location.href = "landing-page";
-}
-
-export default RegisterUser;
